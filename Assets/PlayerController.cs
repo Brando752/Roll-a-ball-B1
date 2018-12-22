d3b45aff6ce603ccf08dabc73126f3e9bb054fc6
Brandon <Brandontylerlai@gmail.com>
2018-12-21 18:22:51 -0800

    b

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,21 +8,33 @@ public class PlayerController : MonoBehaviour {
 
     Vector3 offset;
 
+    public Camera cam; 
+    
+    public float moveSpeed = 1000f;
 	// Use this for initialization
 	void Start () {
 
         rb = this.GetComponent<Rigidbody>();
-        
 
-
-
-
-
-		offset = Camera.main.transform.position - this.transform.position 
+        offset = cam.transform.position - transform.position; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        cam.transform.position = transform.position + offset;
+
+        rb.AddForce(Vector3.forward);
+
+        float hdir = Input.GetAxisRaw("Horizontal");
+        float vdir = Input.GetAxisRaw("Vertical");
+
+        Vector3 directionVector = new Vector3(hdir, 0, vdir);
+        Vector3 unitVector = directionVector.normalized;
+        Vector3 forceVector = unitVector * moveSpeed * Time.deltaTime;
+
+        rb.AddForce(forceVector);
+
+        
+
+    }
 }
