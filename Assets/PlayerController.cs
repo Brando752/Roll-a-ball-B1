ffc1d176802e0f4e7abbed61387ff535db06be08
Brandon <Brandontylerlai@gmail.com>
2018-12-21 17:37:09 -0800

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = this.GetComponent<Rigidbody>();
+        
+
+
+
+
+
+		offset = Camera.main.transform.position - this.transform.position 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
